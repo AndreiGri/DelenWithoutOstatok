6d933e8ecfb3b63b9085adf3cf9a7bfd890d5cab
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-14 23:04:36 +0500

    Добавьте файлы проекта.

---
+++ DelenWithoutOstatok/DelenWithoutOstatok.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки консоли:
+    system("chcp 1251 > nul");
+    int n, k = 1;
+    cout << "Введите опорное число: ";
+    cin >> n;
+start: //Метка
+    if (n % k == 0) {
+        cout << k << " ";
+    }
+    if (k <= n) {
+        k++;
+        goto start;
+    }
+    system("pause > nul");
+    return 0;
+}
